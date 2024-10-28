1c69ecf27b7bb0e00e01785bde0cfb37de2339b9
Jozo_85 <67067963+Jozo233@users.noreply.github.com>
2024-10-28 10:06:43 +0100

    fix

--- Psychedelisf/src/main/java/ml/jozo/tobacco/Tobacco.java
+++ Psychedelisf/src/main/java/ml/jozo/tobacco/Tobacco.java
@@ -2,53 +2,34 @@ package ml.jozo.tobacco;
 
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 
-import ml.jozo.tobacco.categories.Categories;
-import ml.jozo.tobacco.commands.TobaccoCommand;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Tobacco extends JavaPlugin implements SlimefunAddon {
 
-    public static Tobacco instance;
-
-    public static Tobacco inst(){
-        return instance;
-    }
-
-    @Override
-    public JavaPlugin getJavaPlugin(){
-        return this;
-    }
 
     @Override
     public void onEnable() {
+        getLogger().info("####################");
+        getLogger().info("# Psychadelicsf v1 #");
+        getLogger().info("#    by Jozo_85    #");
+        getLogger().info("####################");
 
-        instance = this;
-
-
-        getLogger().info("######################################");
-        getLogger().info("#          Tobaco by Jozo_85         #");
-        getLogger().info("#  SlimeFun addon thats add Tobacco  #");
-        getLogger().info("######################################");
-
-        this.getCommand("tobacco").setExecutor(new TobaccoCommand());
-
-
-        setupSlimefun();
-
+        Config cfg = new Config(this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Login for disabling the plugin
+    }
 
+    @Override
+    public JavaPlugin getJavaPlugin() {
+        return this;
     }
 
     @Override
     public String getBugTrackerURL() {
         return null;
-    }
-
-    private void setupSlimefun(){
-        Categories.set(this);
     }
 }
